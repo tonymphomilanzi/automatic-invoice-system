b3a790a055d17289ac15190d34d059ed80a6e6cc
Tony Milanzi <tonymilanzi01@gmail.com>
2026-03-25 16:06:34 +0200

    Type declaration error fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Playfair_Display, Inter, Geist } from "next/font/google";
-import "@/globals.css";
+import "./globals.css";
 import { cn } from "@/lib/utils";
 
 const playfair = Playfair_Display({ 
